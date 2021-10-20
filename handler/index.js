3525410b21707dc7b3a1c4c58f4697581706f6fe
Infinitybeond1 <88919270+Infinitybeond1@users.noreply.github.com>
2021-10-20 14:17:47 -0500

    change to guild commands

--- handler/index.js
+++ handler/index.js
@@ -41,13 +41,8 @@ module.exports = async (client) => {
         arrayOfSlashCommands.push(file);
     });
     client.on("ready", async () => {
-        // Register for a single guild
-        await client.guilds.cache
-            .get("replace this with your guild id")
-            .commands.set(arrayOfSlashCommands);
-
         // Register for all the guilds the bot is in
-        // await client.application.commands.set(arrayOfSlashCommands);
+        await client.application.commands.set(arrayOfSlashCommands);
     });
 
     // mongoose
